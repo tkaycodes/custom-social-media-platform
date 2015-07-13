9d37312ec4109ed68e9e06104111d2c8f8c8ffae
TABISH KHAN <tabish003@hotmail.com>
2015-07-12 21:05:06 -0700

    edited user show page so user can see own posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    # (current_user.id)
-    # Friendship.find(user: current_user, friend: @user)
     @user = User.find(params[:id])
     @friendship = Friendship.where(user_id: current_user.id, friend_id: @user.id)  
-    if !@friendship.empty? && @friendship.first.state == "accepted"
+    if (!@friendship.empty? && @friendship.first.state == "accepted") || current_user.id==@user.id 
       @posts = @user.posts.order(created_at: :desc)
     else
       @posts = [];
