88238a9a14d6646c56bd7ea990b6483ceb2a9e66
TABISH KHAN <tabish003@hotmail.com>
2015-07-13 21:40:37 -0700

    styled new message show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,21 @@ class CommentsController < ApplicationController
   def create
     @comment=Comment.create(params.require(:comment).permit(:comment, :post_id, :user_id))
     @comment.user_id=current_user.id
-    if @comment.save
-      # logger.warn"CHECK THIS OUT #{@comment.inspect}"
-      # logger.warn"#{@comment.user.inspect}"
-      # logger.warn"CHECK THIS OUT #{@comment.user.email}"
-      redirect_to :back, notice: "comment saved"
-    else
-      redirect_to :back, notice: "comment couldnt be saved"
+    @user=User.find(1)
+      respond_to do|format|
+         format.html{
+          if @comment.save
+            redirect_to :back, notice: "comment saved"
+            # logger.warn"CHECK THIS OUT #{@comment.inspect}"
+            # logger.warn"#{@comment.user.inspect}"
+            # logger.warn"CHECK THIS OUT #{@comment.user.email}"
+          else
+            redirect_to :back, notice: "comment couldnt be saved"
+          end
+        }
+        format.js{}
     end
   end
 
 end
+  
