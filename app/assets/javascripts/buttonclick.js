cb18f4cd491832bc2c5fcfa15ac8cfd61fec4a18
TABISH KHAN <tabish003@hotmail.com>
2015-07-15 15:33:46 -0700

    jquery button clickajax renders correct user profile picture not just default pic

--- app/assets/javascripts/buttonclick.js
+++ app/assets/javascripts/buttonclick.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
   // var friendid  = @friend.id
-
+  var profilepicture = $('#userprofilepicture').html();
+  console.log(profilepicture);
   var currentid= $('#friendshipid').html();
   console.log(currentid);
   var currentemail= $('#friendemail').html();
@@ -20,7 +21,9 @@ $(document).ready(function(){
         console.log(result);
         // return // var friend_id = console.log(result.friend_id);
           $('#'+currentid).hide();
-          $('#friendslist').append("<a href='/user/"+result.friend_id+"'"+"><img width='150' height='150' src='/assets/defaultpic-798db6c38393a747c11c1c37e5e5cbe6dbbb042cb5c26acc714b70653659417d.jpg'alt='Defaultpic 798db6c38393a747c11c1c37e5e5cbe6dbbb042cb5c26acc714b70653659417d'></a>"+currentemail+"<a rel='nofollow' data-method='DELETE' href='/friendships/"+currentid+"'"+">Remove Friend</a>");
+          $('#friendslist').append("<a href='/user/"+result.friend_id+"'"+"><img width='150' height='150' src='"+profilepicture+"'></a>"+currentemail+"<a rel='nofollow' data-method='DELETE' href='/friendships/"+currentid+"'"+">Remove Friend</a>");
+
+          // $('#friendslist').append("<a href='/user/"+result.friend_id+"'"+"><img width='150' height='150' src='/assets/defaultpic-798db6c38393a747c11c1c37e5e5cbe6dbbb042cb5c26acc714b70653659417d.jpg'alt='Defaultpic 798db6c38393a747c11c1c37e5e5cbe6dbbb042cb5c26acc714b70653659417d'></a>"+currentemail+"<a rel='nofollow' data-method='DELETE' href='/friendships/"+currentid+"'"+">Remove Friend</a>");
           // $('#friendslist').append("<a href='/user/"+result.friend_id+"'"+">+"+"'"+friendid.profile_picture+"'+"+currentemail+"'"+"<a rel='nofollow' data-method='DELETE' href='/friendships/"+currentid+"'"+">Remove Friend</a>"                         
        }
     });
--- app/assets/javascripts/sendmessage.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $('#sendmessagebtn').click(function(event){
-      event.preventDefault();
-
-  });
-});
