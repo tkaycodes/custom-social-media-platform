fe627536b8d39bbcf23963ef95f8e6378c7a6d7a
TABISH KHAN <tabish003@hotmail.com>
2015-07-15 16:44:55 -0700

    added status column to message model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714211349) do
+ActiveRecord::Schema.define(version: 20150715234209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150714211349) do
     t.string   "recipient_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "status"
   end
 
   create_table "posts", force: :cascade do |t|
