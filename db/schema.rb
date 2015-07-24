ef22ef29a3a5c23801b8b21c2448bf8e4519fb9e
TABISH KHAN <tabish003@hotmail.com>
2015-07-24 16:26:38 -0700

    added status to taggeduser table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722020510) do
+ActiveRecord::Schema.define(version: 20150724222331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20150722020510) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
   add_index "taggedusers", ["post_id"], name: "index_taggedusers_on_post_id", using: :btree
