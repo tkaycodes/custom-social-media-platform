c88a9fb6eff2a8841206567303ee206c13025fa2
TABISH KHAN <tabish003@hotmail.com>
2015-08-08 13:54:24 -0700

    added username to user model, login with username or email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,9 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name
-    devise_parameter_sanitizer.for(:sign_up) << :last_name
-    devise_parameter_sanitizer.for(:sign_up) << :profile_picture
-    devise_parameter_sanitizer.for(:sign_up) << :username
-    devise_parameter_sanitizer.for(:sign_in) << :username
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:profile_picture, :first_name, :last_name, :username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
 
   def remove_header
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+
   has_attached_file :profile_picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path('defaultpic.jpg');
   validates_attachment_content_type :profile_picture, :content_type => /\Aimage\/.*\Z/ 
-  
+  validates :username, uniqueness: true;
   # after_create :set_default_avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -18,7 +20,7 @@ class User < ActiveRecord::Base
   has_many :recieved_messages, class_name: "Message", foreign_key: :recipient_id
 
 
-  validates :email, uniqueness: true
+  # validates :email, uniqueness: true
 
   has_many :likes
   has_many :liked_posts, through: :likes, source: :post
@@ -33,6 +35,15 @@ class User < ActiveRecord::Base
   # def set_default_avatar
   #   self.profile_picture = 'defaultpic.jpg'
   # end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+  conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions.to_hash).first
+    end
+  end
 
 
 end
